982df71a2a89f105bf038d7a0b2f6d52c593da37
CGTNathan <cgtnathan@gmail.com>
2015-07-04 01:21:38 -0400

    randp

--- chat-plugins/info.js
+++ chat-plugins/info.js
@@ -173,50 +173,104 @@ var commands = exports.commands = {
 	 * Data Search Tools
 	 *********************************************************/
 	
-	randp: function (room, user) {
+	randp: function (target, room, user) {
+		var fs = require('fs');
+		var fileName = "pokedex.js";
 		if (!this.canBroadcast()) return;
 	
-		var randP = '';
-		var x = Math.floor((Math.random() * 721) + 1);
-		var pokeNum = parseInt(x);
-		var targetId = toId(pokeNum);
-		if (targetId === '' + parseInt(pokeNum)) {
-			for (var p in Tools.data.Pokedex) {
-				var pokemon = Tools.getTemplate(p);
-				if (pokemon.num === parseInt(pokeNum)) {
-					pokeNum = pokemon.species;
-					targetId = pokemon.id;
-					break;
-				}
-			}
-		}	
-		var newTargets = Tools.dataSearch(pokeNum);
-		if (newTargets && newTargets.length) {
-			for (var i = 0; i < newTargets.length; ++i) {
-				var pokemon = Tools.getTemplate(newTargets[0].name);
-				var pokeName = pokemon.species;
-				var pokeType1 = pokemon.types[0];
-				var pokeType2 = pokemon.types[1];
-				var pokeGen = pokemon.gen;
-				var pokeHp = pokemon.baseStats['hp'];
-				var pokeAtk = pokemon.baseStats['atk'];
-				var pokeDef = pokemon.baseStats['def'];
-				var pokeSpa = pokemon.baseStats['spa'];
-				var pokeSpd = pokemon.baseStats['spd'];
-				var pokeSpe = pokemon.baseStats['spe'];
-				var pokeBst = pokemon.baseStats['hp'] + pokemon.baseStats.atk + pokemon.baseStats.def + pokemon.baseStats.spa + pokemon.baseStats.spd + pokemon.baseStats.spe;
-				var pokeColor = pokemon.color;
-				var pokeEgg = pokemon.eggGroups.join(", ");
-			}
-		}
-		if (pokemon.types.length === 2) {
-			randP = '<table><tr><td><img src="http://play.pokemonshowdown.com/sprites/bw/' + string(pokeName).toLowerCase().replace(/[^a-z0-9]+/g, '') + '.png" height="96" 	width="96"></td><td><b>Name: </b>' + pokeName + '<br/><b>Type: </b><img src="http://play.pokemonshowdown.com/sprites/types/' + pokeType1 + '.png" width="32" height="14"> <img src="http://play.pokemonshowdown.com/sprites/types/' + pokeType2 + '.png" width="32" height="14"><br/><b>Stats: </b> HP ' + pokeHp + ' / Atk ' + pokeAtk + ' / Def ' + pokeDef + ' / SpA ' + pokeSpa + ' / SpD ' + pokeSpd + ' / Spe ' + pokeSpe + ' / BST ' + pokeBst + '<br/><b>Color: </b><font color="' + pokeColor + '">' + pokeColor + '</font><br/><b>Egg Group: </b>' + pokeEgg + '</td></tr></table>'
+		if (/shiny/.test(target)) {
+			var shiny = true;
+		};
+		if (/kanto/.test(target) || /gen 1/.test(target)) {
+			var x = Math.floor((Math.random() * 173) + 2);
+		} else if (/jhoto/.test(target) || /gen 2/.test(target)) {
+			var x = Math.floor((Math.random() * 105) + 175);
+		} else if (/hoenn/.test(target) || /gen 3/.test(target)) {
+			var x = Math.floor((Math.random() * 105) + 280);
 		} else {
-			randP = '<table><tr><td><img src="http://play.pokemonshowdown.com/sprites/bw/' + string(pokeName).toLowerCase().replace(/[^a-z0-9]+/g, '') + '.png" height="96" width="96"></td><td><b>Name: </b>' + pokeName + '<br/><b>Type: </b><img src="http://play.pokemonshowdown.com/sprites/types/' + pokeType1 + '.png" width="32" height="14"><br/><b>Stats: </b> HP ' + pokeHp + ' / Atk ' + pokeAtk + ' / Def ' + pokeDef + ' / SpA ' + pokeSpa + ' / SpD ' + pokeSpd + ' / Spe ' + pokeSpe + ' / BST ' + pokeBst + '<br/><b>Color: </b><font color="' + pokeColor + '">' + pokeColor + '</font><br/><b>Egg Group: </b>' + pokeEgg + '</td></tr></table>'
+			var x = Math.floor((Math.random() * 855) + 2);
 		}
+		var randP = '';
+		var pokeNum = parseInt(x);
+		
+		var pokedex = fs.readFileSync('./data/pokedex.js').toString().split("\n");
+		var pokemon = (pokedex[x]);
+		var speciesIndex1 = pokemon.indexOf('species:"') + 9; var speciesIndex2 = pokemon.indexOf('",', speciesIndex1);
+		var pokeName = pokemon.slice(speciesIndex1,speciesIndex2);
+		var type1Index1 = pokemon.indexOf(',types:["') + 9; var type1Index2 = pokemon.indexOf('"],', type1Index1);
+		var pokeType2 = ''
+		if (/,/.test(pokemon.slice(type1Index1, type1Index2))) {
+			var type1Index2 = pokemon.indexOf('","', type1Index1);
+			var type2Index1 = pokemon.indexOf('","', type1Index1) + 3; var type2Index2 = pokemon.indexOf('"],', type2Index1);
+			var pokeType2 = '<img src="http://play.pokemonshowdown.com/sprites/types/' + pokemon.slice(type2Index1, type2Index2) + '.png" width="32" height="14">'
+		};
+		var pokeType1 = '<img src="http://play.pokemonshowdown.com/sprites/types/' + pokemon.slice(type1Index1, type1Index2) + '.png" width="32" height="14">'
+		var ability1Index1 = pokemon.indexOf(',abilities:{0:"') + 15; var ability1Index2 = pokemon.indexOf('"},h', ability1Index1);	
+		var pokeAbility2 = ''
+		var pokeAbility3 = ''
+		if (/",1:"/.test(pokemon.slice(ability1Index1, ability1Index2))) {
+			if (/",H:"/.test(pokemon.slice(ability1Index1, ability1Index2))) {
+				var ability1Index2 = pokemon.indexOf('",H:"', ability1Index1);
+				var ability3Index1 = pokemon.indexOf('",H:"', ability1Index1) + 5; var ability3Index2 = pokemon.indexOf('"', ability3Index1);
+				var pokeAbility3 = ', ' + pokemon.slice(ability3Index1, ability3Index2);
+			};
+			var ability1Index2 = pokemon.indexOf('",1:"', ability1Index1);
+			var ability2Index1 = pokemon.indexOf('",1:"', ability1Index1) + 5; var ability2Index2 = pokemon.indexOf('"', ability2Index1);
+			var pokeAbility2 = ', ' + pokemon.slice(ability2Index1, ability2Index2);
+		};
+		var ability1Index2 = pokemon.indexOf('"', ability1Index1);
+		var pokeAbility1 = pokemon.slice(ability1Index1, ability1Index2)
+		var hpIndex1 = pokemon.indexOf('hp:') + 3; var hpIndex2 = pokemon.indexOf(',', hpIndex1);
+		var pokeHp = parseInt(pokemon.slice(hpIndex1, hpIndex2));
+		var atkIndex1 = pokemon.indexOf('atk:') + 4; var atkIndex2 = pokemon.indexOf(',', atkIndex1);
+		var pokeAtk = parseInt(pokemon.slice(atkIndex1, atkIndex2));
+		var defIndex1 = pokemon.indexOf('def:') + 4; var defIndex2 = pokemon.indexOf(',', defIndex1);
+		var pokeDef = parseInt(pokemon.slice(defIndex1, defIndex2));
+		var spaIndex1 = pokemon.indexOf('spa:') + 4; var spaIndex2 = pokemon.indexOf(',', spaIndex1);
+		var pokeSpa = parseInt(pokemon.slice(spaIndex1, spaIndex2));
+		var spdIndex1 = pokemon.indexOf('spd:') + 4; var spdIndex2 = pokemon.indexOf(',', spdIndex1);
+		var pokeSpd = parseInt(pokemon.slice(spdIndex1, spdIndex2));
+		var speIndex1 = pokemon.indexOf('spe:') + 4; var speIndex2 = pokemon.indexOf('}', speIndex1);
+		var pokeSpe = parseInt(pokemon.slice(speIndex1, speIndex2));
+		var pokeBst = pokeHp + pokeAtk + pokeDef + pokeSpa + pokeSpd + pokeSpe;
+		var pokeStats = 'HP ' + pokeHp + ' / Atk ' + pokeAtk + ' / Def ' + pokeDef + ' / SpA ' + pokeSpa + ' / SpD ' + pokeSpd + ' / Spe ' + pokeSpe + ' / BST ' + pokeBst;
+		var colorIndex1 = pokemon.indexOf(',color:"') + 8; var colorIndex2 = pokemon.indexOf('",', colorIndex1);
+		var pokeColor = pokemon.slice(colorIndex1, colorIndex2);
+		var egg1Index1 = pokemon.indexOf(',eggGroups:["') + 13; var egg1Index2 = pokemon.indexOf('"]', egg1Index1);
+		var pokeEgg2 = ""
+		if (/,/.test(pokemon.slice(egg1Index1, egg1Index2))) {
+			var egg1Index2 = pokemon.indexOf('","', egg1Index1);
+			var egg2Index1 = pokemon.indexOf('","', egg1Index1) + 3; var egg2Index2 = pokemon.indexOf('"]', egg2Index1);
+			var pokeEgg2 = ", " + pokemon.slice(egg2Index1, egg2Index2);
+		}
+		var pokeEgg1 = pokemon.slice(egg1Index1, egg1Index2);
+		
+		if (shiny === true) {
+			if (pokeName === "Ho-Oh" || pokeName === "Nidoran-F" || pokeName === "Nidoran-M" || pokeName === "Farfetch'd" || pokeName === "Porygon-Z") {
+			randP = '<table><tr><td><img src="http://play.pokemonshowdown.com/sprites/bw-shiny/' + string(pokeName).toLowerCase().replace(/[-]+/g, '').replace(/[']+/g, '') + '.png" height="96" width="96"></td><td><b>Name: </b>' + pokeName + '<br/><b>Type(s): </b>' + pokeType1 + ' ' + pokeType2 + '<br/><b>Ability: </b>' + pokeAbility1 + pokeAbility2 + pokeAbility3 + '<br/><b>Stats: </b>' + pokeStats + '<br/><b>Color: </b><font color="' + pokeColor + '">' + pokeColor + '</font><br/><b>Egg Group(s): </b>' + pokeEgg1 + pokeEgg2 + '</td></tr></table>';
+			} else if (pokeName === "Basculin-Blue-Striped") {
+			randP = '<table><tr><td><img src="http://play.pokemonshowdown.com/sprites/bw-shiny/basculin-bluestriped.png" height="96" width="96"></td><td><b>Name: </b>' + pokeName + '<br/><b>Type(s): </b>' + pokeType1 + ' ' + pokeType2 + '<br/><b>Ability: </b>' + pokeAbility1 + pokeAbility2 + pokeAbility3 + '<br/><b>Stats: </b>' + pokeStats + '<br/><b>Color: </b><font color="' + pokeColor + '">' + pokeColor + '</font><br/><b>Egg Group(s): </b>' + pokeEgg1 + pokeEgg2 + '</td></tr></table>';
+			} else if (pokeName === "Pichu-Spiky-eared") {
+			randP = '<table><tr><td><img src="http://play.pokemonshowdown.com/sprites/bw-shiny/pichu-spikyeared.png" height="96" width="96"></td><td><b>Name: </b>' + pokeName + '<br/><b>Type(s): </b>' + pokeType1 + ' ' + pokeType2 + '<br/><b>Ability: </b>' + pokeAbility1 + pokeAbility2 + pokeAbility3 + '<br/><b>Stats: </b>' + pokeStats + '<br/><b>Color: </b><font color="' + pokeColor + '">' + pokeColor + '</font><br/><b>Egg Group(s): </b>' + pokeEgg1 + pokeEgg2 + '</td></tr></table>';
+			} else {
+			randP = '<table><tr><td><img src="http://play.pokemonshowdown.com/sprites/bw-shiny/' + string(pokeName).toLowerCase().replace(/[ ]+/g, '').replace(/[.]+/g, '').replace(/[']+/g, '') + '.png" height="96" width="96"></td><td><b>Name: </b>' + pokeName + '<br/><b>Type(s): </b>' + pokeType1 + ' ' + pokeType2 + '<br/><b>Ability: </b>' + pokeAbility1 + pokeAbility2 + pokeAbility3 + '<br/><b>Stats: </b>' + pokeStats + '<br/><b>Color: </b><font color="' + pokeColor + '">' + pokeColor + '</font><br/><b>Egg Group(s): </b>' + pokeEgg1 + pokeEgg2 + '</td></tr></table>';
+			};
+		} else {;
+			if (pokeName === "Ho-Oh" || pokeName === "Nidoran-F" || pokeName === "Nidoran-M" || pokeName === "Farfetch'd" || pokeName === "Porygon-Z") {
+				randP = '<table><tr><td><img src="http://play.pokemonshowdown.com/sprites/bw/' + string(pokeName).toLowerCase().replace(/[-]+/g, '').replace(/[']+/g, '') + '.png" height="96" width="96"></td><td><b>Name: </b>' + pokeName + '<br/><b>Type(s): </b>' + pokeType1 + ' ' + pokeType2 + '<br/><b>Ability: </b>' + pokeAbility1 + pokeAbility2 + pokeAbility3 + '<br/><b>Stats: </b>' + pokeStats + '<br/><b>Color: </b><font color="' + pokeColor + '">' + pokeColor + '</font><br/><b>Egg Group(s): </b>' + pokeEgg1 + pokeEgg2 + '</td></tr></table>';
+			} else if (pokeName === "Basculin-Blue-Striped") {
+				randP = '<table><tr><td><img src="http://play.pokemonshowdown.com/sprites/bw/basculin-bluestriped.png" height="96" width="96"></td><td><b>Name: </b>' + pokeName + '<br/><b>Type(s): </b>' + pokeType1 + ' ' + pokeType2 + '<br/><b>Ability: </b>' + pokeAbility1 + pokeAbility2 + pokeAbility3 + '<br/><b>Stats: </b>' + pokeStats + '<br/><b>Color: </b><font color="' + pokeColor + '">' + pokeColor + '</font><br/><b>Egg Group(s): </b>' + pokeEgg1 + pokeEgg2 + '</td></tr></table>';
+			} else if (pokeName === "Pichu-Spiky-eared") {
+			randP = '<table><tr><td><img src="http://play.pokemonshowdown.com/sprites/bw/pichu-spikyeared.png" height="96" width="96"></td><td><b>Name: </b>' + pokeName + '<br/><b>Type(s): </b>' + pokeType1 + ' ' + pokeType2 + '<br/><b>Ability: </b>' + pokeAbility1 + pokeAbility2 + pokeAbility3 + '<br/><b>Stats: </b>' + pokeStats + '<br/><b>Color: </b><font color="' + pokeColor + '">' + pokeColor + '</font><br/><b>Egg Group(s): </b>' + pokeEgg1 + pokeEgg2 + '</td></tr></table>';
+			} else if (pokeName === "Missingno.") {
+				randP = '<table><tr><td><img src="http://play.pokemonshowdown.com/sprites/bw/missingno.png" height="96" width="96"></td><td><b>Name: </b>' + pokeName + '<br/><b>Type(s): </b>' + pokeType1 + ' ' + pokeType2 + '<br/><b>Ability: </b>None<br/><b>Stats: </b>' + pokeStats + '<br/><b>Color: </b><font color="' + pokeColor + '">' + pokeColor + '</font><br/><b>Egg Group(s): </b>' + pokeEgg1 + pokeEgg2 + '</td></tr></table>';
+			} else {
+				randP = '<table><tr><td><img src="http://play.pokemonshowdown.com/sprites/bw/' + string(pokeName).toLowerCase().replace(/[ ]+/g, '').replace(/[.]+/g, '').replace(/[']+/g, '') + '.png" height="96" width="96"></td><td><b>Name: </b>' + pokeName + '<br/><b>Type(s): </b>' + pokeType1 + ' ' + pokeType2 + '<br/><b>Ability: </b>' + pokeAbility1 + pokeAbility2 + pokeAbility3 + '<br/><b>Stats: </b>' + pokeStats + '<br/><b>Color: </b><font color="' + pokeColor + '">' + pokeColor + '</font><br/><b>Egg Group(s): </b>' + pokeEgg1 + pokeEgg2 + '</td></tr></table>';
+			};
+		};
 		this.sendReplyBox(randP);
 	},
-
+	
 	pstats: 'data',
 	stats: 'data',
 	dex: 'data',
